018e25d9e8325eb3b9bf3cb0fab8934d61d943f1
jkim3615 <42694714+jkim3615@users.noreply.github.com>
2020-06-03 22:54:59 +0900

    Added V2
    This time, I used def function to define the total budget. If I have a time, I will define every aspect of the functions to make it more efficient.

--- budgetcalc.py
+++ budgetcalc.py
@@ -2,32 +2,38 @@
 # By Jack Kim
 # Budget Calculator that supposed to work even without WiFi or Network
 
+# Calendar
+
+
 # Monthly Budget
 
-n=int(input("Enter your monthly budget:  "))
-if n>0:
-    print("Your monthly budget is", n, "krw.")
+def total_budget():
+    n=int(input("Enter your monthly budget:  "))
+    if n>0:
+        print("Your monthly budget is", n, "krw.")
 
-# Meal Budget
+    # Meal Budget
 
-x=int(input("Enter your meal budget:  "))
-if x>0 or x<n:
-    print("Your meal budget is", x, "krw.")
+    x=int(input("Enter your meal budget:  "))
+    if x>0 or x<n:
+        print("Your meal budget is", x, "krw.")
 
-# Transportation Budget
+    # Transportation Budget
 
-y=int(input("Enter your transportation budget:  "))
-if y>0 or y<n:
-    print("Your transportation budget is", y, "krw.")
+    y=int(input("Enter your transportation budget:  "))
+    if y>0 or y<n:
+        print("Your transportation budget is", y, "krw.")
 
-# Social Budget
+    # Social Budget
 
-h=int(input("Enter your social budget:  "))
-if h>0 or h<n:
-    print("Your social budget is", h, "krw.")
+    h=int(input("Enter your social budget:  "))
+    if h>0 or h<n:
+        print("Your social budget is", h, "krw.")
 
-# Remaining Budget
+    # Remaining Budget
 
-total_budget = n-x-y-h
-txt = "The total budget is {} krw."
-print(txt.format(total_budget))
+    total_budget = n-x-y-h
+    txt = "The total budget is {} krw."
+    print(txt.format(total_budget))
+
+total_budget()
